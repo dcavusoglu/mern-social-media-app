1f9a9c95d91d080201b5551cbfa497544254bcb6
Duygu Cavusoglu <cvsgl.dyg@gmail.com>
2022-06-15 14:02:00 +0300

    form added

--- server/index.js
+++ server/index.js
@@ -7,12 +7,12 @@ import heroRoutes from './routes/heros.js';
 
 const app = express();
 
-app.use('/heros', heroRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use('/heros', heroRoutes);
 
 // https://www.mangodb.com/cloud/atlas
 
